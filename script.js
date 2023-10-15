33ade24fffdd6b7d4e3e80498aa988e76db605f3
Affaan <affaanshaikh6@gmail.com>
2023-10-16 01:11:52 +0530

    almost there

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let menu= document.querySelector('#menu-btn');
-let navbar= document.querySelector('.navbar');
-
-menu.onlick= () =>{
+let menu = document.querySelector('menu-btn');
+let navbar = document.querySelector('.navbar');
+  
+menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
@@ -9,5 +9,4 @@ menu.onlick= () =>{
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-
 }
