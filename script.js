058e2598f22ef12dc946ae58c001bbf7217da298
Affaan <affaanshaikh6@gmail.com>
2023-10-22 00:22:07 +0530

    Tryinf to add login

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let menu = document.querySelector('menu-btn');
 let navbar = document.querySelector('.navbar');
   
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+    menu.classList.add('fa-times');
+    navbar.classList.add('active');
 }
 
 window.onscroll = () =>{
